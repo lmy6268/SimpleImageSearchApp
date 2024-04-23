1c5848841c9dfe336100f47b3c57f00751876aa3
kyuyeonlee <lmy6268@gmail.com>
2024-04-24 00:09:37 +0900

    test: Pixabay API 테스트 -> Mockito 사용 (L09)
    [Mockito for Network Test]
    - Http 라이브러리 내부에는 Client 객체가 존재하는데, 테스트를 위해, 이 객체를 가짜(Mockito)데이터로 교체하여, 네트워크 통신을 테스트한다.
      * 참고 : https://docs.flutter.dev/cookbook/testing/unit/mocking
    
    - mockClient 객체를 이용하여 네트워크 테스트를 진행한다.

--- lib/data/api.dart
+++ lib/data/api.dart
@@ -5,13 +5,16 @@ import '../model/Photo.dart';
 import 'package:http/http.dart' as http;
 
 class PixabayAPI implements PhotoApiRepository {
-  final String baseUrl = "https://pixabay.com/api/";
-  final String key = "17159641-1bf10cc7b7513d39c89bb809d";
+  static const String baseUrl = "https://pixabay.com/api/";
+  static const String key = "17159641-1bf10cc7b7513d39c89bb809d";
 
+  //{} 는 옵션 인자를 의미한다.
   @override
-  Future<List<Photo>> fetch(String query) async {
+  Future<List<Photo>> fetch(String query, {http.Client? client}) async {
+    client ??= http.Client(); //client가 Null인경우에만, 이렇게 초기화해준다.
+
     final String url = '$baseUrl?key=$key&q=$query&image_type=photo';
-    final response = await http.get(Uri.parse(url));
+    final response = await client.get(Uri.parse(url));
     Map<String, dynamic> jsonResponse = jsonDecode(response.body);
     Iterable hits = jsonResponse['hits']; //아직은 Map 형태
     return hits.map((e) => Photo.fromJson(e)).toList();
